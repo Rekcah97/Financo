24dd8ff7967eaba293ec4880a7090245db360924
Arpit Regmi <arpitregmi121@gmail.com>
2026-02-24 10:00:54 +0545

    cleanup expired OTP

--- backend/jobs/cleanupExpiredEntries.utils.js
+++ backend/jobs/cleanupExpiredEntries.utils.js
@@ -2,12 +2,19 @@ import prisma from "../config/db.config.js";
 
 export const cleanupExpiredEntries = async (req, res) => {
   try {
-    const result = await prisma.refreshToken.deleteMany({
+    const result1 = await prisma.refreshToken.deleteMany({
       where: {
         expiresAt: { lt: new Date() },
       },
     });
-    console.log(`Cleaned up ${result.count} expired Refersh Token`);
+    const result2 = await prisma.verificationCode.deleteMany({
+      where: {
+        expiresAt: { lt: new Date() },
+      },
+    });
+    console.log(
+      `Cleaned up ${result1.count} expired Refersh Token and ${result2.count} expired verification code`,
+    );
   } catch (err) {
     console.log("Error in cleaning up Expired Entires");
   }
