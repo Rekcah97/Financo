7a04fc9e9eb0b776b51515796f17703e8eb2d139
Rekcah <arpitregmi121@gmail.com>
2026-03-10 11:23:19 +0545

    resend fix

--- backend/utils/sendVerificationOtp.js
+++ backend/utils/sendVerificationOtp.js
@@ -1,5 +1,6 @@
 import bcrypt from "bcrypt";
 import prisma from "../config/db.config.js";
+import { Resend } from "resend";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
@@ -20,12 +21,18 @@ export const sendVerificationOTP = async (id, email) => {
       },
     });
 
-    await resend.emails.send({
+    const { data, error } = await resend.emails.send({
       from: "noreply@arpitregmi.com.np",
       to: email,
       subject: "Verify Your Email",
       html: `<p>Enter <b>${otp}</b> in the Financo application to verify your Email Address And complete the verification process.</p><p>The OTP will expire in <b>1 Hour</b>.</p>`,
     });
+
+    if (error) {
+      console.log("Resend error:", error);
+      return { success: false };
+    }
+    console.log("OTP email sent successfully", data);
 
     return { success: true };
   } catch (err) {
