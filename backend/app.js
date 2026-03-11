720debad770760b25dd38ff62c95eac1b4b63d39
Rekcah <arpitregmi121@gmail.com>
2026-03-11 18:41:09 +0545

    ratelimiting in auth fixed and helmet

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { rateLimit } from "express-rate-limit";
 import Routes from "./Routes/index.js";
+import helmet from "helmet";
 
 const timeFor1Hr = 60 * 60 * 1000;
 const MAX_REQUEST_AUTH = 20;
@@ -8,14 +9,6 @@ const MAX_REQUEST_GENERAL = 1000;
 const app = express();
 
 //rate limiters
-const generalLimiter = rateLimit({
-  max: MAX_REQUEST_GENERAL,
-  windowMs: timeFor1Hr,
-  skip: (req) => req.path.startsWith("/api/auth"),
-  message:
-    "we have reached too many request from this ip address. pls retry in 1 hour",
-});
-
 const authLimiter = rateLimit({
   max: MAX_REQUEST_AUTH,
   windowMs: timeFor1Hr,
@@ -23,10 +16,19 @@ const authLimiter = rateLimit({
     "we have reached too many auth request from this ip address. pls retry in 1 hour",
 });
 
+const generalLimiter = rateLimit({
+  max: MAX_REQUEST_GENERAL,
+  windowMs: timeFor1Hr,
+  skip: (req) => req.originalUrl.startsWith("/api/auth"),
+  message:
+    "we have reached too many request from this ip address. pls retry in 1 hour",
+});
+
 //middleware
 app.use(express.json());
+app.use(helmet());
 app.use("/api/auth", authLimiter);
-app.use("/api", generalLimiter);
+app.use("/", generalLimiter);
 
 //routes
 app.use("/api", Routes);
