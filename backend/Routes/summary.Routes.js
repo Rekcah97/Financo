91110e756bbd4995beac83cfdb4efb339ed178ff
Rekcah <arpitregmi121@gmail.com>
2026-03-11 12:48:27 +0545

    summary route

--- backend/Routes/summary.Routes.js
+++ backend/Routes/summary.Routes.js
@@ -1,10 +1,20 @@
 import { Router } from "express";
 import { verifyJWT } from "../middleware/verifyJWT.middleware.js";
-import { summaryDetails } from "../controllers/summary.Controller.js";
+import {
+  balanceDetails,
+  summaryDetails,
+  summaryDetailsbyDate,
+} from "../controllers/summary.Controller.js";
 
 const router = Router();
 
 //Route 1
 router.get("/fetch", verifyJWT, summaryDetails);
+
+//Route 2
+router.get("/fetchByDate", verifyJWT, summaryDetailsbyDate);
+
+//Route 3
+router.get("/fetchBalance", verifyJWT, balanceDetails);
 
 export default router;
--- backend/controllers/summary.Controller.js
+++ backend/controllers/summary.Controller.js
@@ -1,3 +1,4 @@
+import prisma from "../config/db.config.js";
 import {
   allocatedSavingMoney,
   totalSavingMoney,
@@ -8,18 +9,114 @@ export const summaryDetails = async (req, res) => {
   try {
     const userId = Number(req.user.id);
 
+    const income = await prisma.transaction.aggregate({
+      _sum: { amount: true },
+      where: {
+        userId,
+        category: { is: { type: "income" } },
+      },
+    });
+
+    const expense = await prisma.transaction.aggregate({
+      _sum: { amount: true },
+      where: {
+        userId,
+        category: { is: { type: "expense" } },
+      },
+    });
+
+    const saving = await prisma.transaction.aggregate({
+      _sum: { amount: true },
+      where: {
+        userId,
+        category: { is: { type: "saving" } },
+      },
+    });
+
+    const incomeAmount = income._sum.amount ?? 0;
+    const expenseAmount = expense._sum.amount ?? 0;
+    const savingAmount = saving._sum.amount ?? 0;
+
+    return res
+      .status(200)
+      .json({ success: true, incomeAmount, expenseAmount, savingAmount });
+  } catch (err) {
+    return res
+      .status(500)
+      .json({ success: false, msg: "Internal Server Error" });
+  }
+};
+
+export const balanceDetails = async (req, res) => {
+  try {
+    const userId = Number(req.user.id);
+
     const totalSavingAmount = await totalSavingMoney(userId);
     const allocatedAmount = await allocatedSavingMoney(userId);
     const unallocatedAmount = await unallocatedSavingMoney(userId);
 
+    return res.status(200).json({
+      success: true,
+      totalSavingAmount,
+      allocatedAmount,
+      unallocatedAmount,
+    });
+  } catch (err) {
+    return res
+      .status(500)
+      .json({ success: false, msg: "Internal Server Error" });
+  }
+};
+
+export const summaryDetailsbyDate = async (req, res) => {
+  try {
+    const userId = Number(req.user.id);
+    const { start, end } = req.query;
+
+    if (!start || !end) {
+      return res
+        .status(400)
+        .json({ success: false, msg: "Start and end are requireds" });
+    }
+
+    const startDate = new Date(start);
+    const endDate = new Date(end);
+    endDate.setHours(23, 59, 59, 999);
+
+    const income = await prisma.transaction.aggregate({
+      _sum: { amount: true },
+      where: {
+        userId,
+        category: { is: { type: "income" } },
+        date: { gte: startDate, lte: endDate },
+      },
+    });
+
+    const expense = await prisma.transaction.aggregate({
+      _sum: { amount: true },
+      where: {
+        userId,
+        category: { is: { type: "expense" } },
+        date: { gte: startDate, lte: endDate },
+      },
+    });
+
+    const saving = await prisma.transaction.aggregate({
+      _sum: { amount: true },
+      where: {
+        userId,
+        category: { is: { type: "saving" } },
+        date: { gte: startDate, lte: endDate },
+      },
+    });
+
+    const incomeAmount = income._sum.amount ?? 0;
+    const expenseAmount = expense._sum.amount ?? 0;
+    const savingAmount = saving._sum.amount ?? 0;
+
     return res
       .status(200)
-      .json({
-        success: true,
-        totalSavingAmount,
-        allocatedAmount,
-        unallocatedAmount,
-      });
+      .json({ success: true, incomeAmount, expenseAmount, savingAmount });
   } catch (err) {
     return res
       .status(500)
