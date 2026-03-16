7113d551490895e5ca09bbaf6d11b325e2b4d266
Arpit Regmi <arpitregmi121@gmail.com>
2026-03-16 17:01:04 +0545

    Feat : test for refresh Token

--- backend/tests/auth.test.js
+++ backend/tests/auth.test.js
@@ -9,7 +9,7 @@ jestObj.mock("../utils/sendVerificationOtp.utils.js", () => ({
   sendVerificationOTP: jest.fn().mockResolvedValue({ success: true }),
 }));
 
-const testUser = {
+let testUser = {
   name: "test User",
   email: "testemail@gmail.com",
   password: "testUser@123",
@@ -43,6 +43,16 @@ const deleteTestUserData = async () => {
   }
 };
 
+const saveUser = async () => {
+  const user = await prisma.user.findUnique({
+    where: {
+      email: testUser.email,
+    },
+  });
+
+  testUser.id = user.id;
+};
+
 const signupResponse = async (name, email, password) => {
   const result = await test(app).post("/api/auth/signup").send({
     name,
@@ -53,9 +63,18 @@ const signupResponse = async (name, email, password) => {
   return result;
 };
 
+const refreshResponse = async (token) => {
+  const result = await test(app).post("/api/auth/refresh").send({
+    refreshToken: token,
+  });
+
+  return result;
+};
+
 //Route for signup
 describe("POST /api/auth/signup", () => {
   beforeAll(deleteTestUserData);
+  afterAll(saveUser);
 
   //test 1
   it("Should create a user and Return 201", async () => {
@@ -64,6 +83,9 @@ describe("POST /api/auth/signup", () => {
       testUser.email,
       testUser.password,
     );
+
+    testUser.refreshToken = response.body.refreshToken;
+
     expect(response.status).toBe(201);
     expect(response.body.success).toBe(true);
     expect(response.body.accessToken).toBeDefined();
@@ -88,6 +110,58 @@ describe("POST /api/auth/signup", () => {
       testUser.password,
     );
     expect(response.status).toBe(409);
+    expect(response.body.success).toBe(false);
+  });
+});
+
+//Route for refresh Access Token
+describe("POST /api/auth/refresh", () => {
+  //test 1
+  it("Create AccessToken and return 201", async () => {
+    const refreshToken = testUser.refreshToken;
+    const response = await refreshResponse(refreshToken);
+
+    testUser.refreshToken = response.body.refreshToken;
+    expect(response.status).toBe(201);
+    expect(response.body.success).toBe(true);
+    expect(response.body.accessToken).toBeDefined();
+  });
+
+  //test 2
+  it("Return 400 if refreshToken is empty", async () => {
+    const refreshToken = "";
+    const response = await refreshResponse(refreshToken);
+    expect(response.status).toBe(400);
+    expect(response.body.success).toBe(false);
+  });
+
+  //test 3
+  it("Return 401 if refreshToken is expired", async () => {
+    const refreshToken = testUser.refreshToken;
+
+    const ONE_DAY_MS = 24 * 60 * 60 * 1000;
+    const oneDayBeforeNow = Date.now() - ONE_DAY_MS;
+
+    await prisma.refreshToken.update({
+      where: {
+        token: refreshToken,
+      },
+      data: {
+        expiresAt: new Date(oneDayBeforeNow),
+      },
+    });
+
+    const response = await refreshResponse(refreshToken);
+    expect(response.status).toBe(401);
+    expect(response.body.success).toBe(false);
+  });
+
+  //test 4
+  it("Return 401 if token doesnot exit in database", async () => {
+    const refreshToken = testUser.refreshToken;
+    const response = await refreshResponse(refreshToken);
+
+    expect(response.status).toBe(401);
     expect(response.body.success).toBe(false);
   });
 });
