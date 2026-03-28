e349a7be444667ba5ccfab5162ddf9c81f19ea33
Arpit Regmi <arpitregmi121@gmail.com>
2026-03-28 10:08:42 +0545

    Refactor: Proper Routing using react-router-dom

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,4 @@
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import PrivateNavBar from "./components/NavBar/PrivateNavBar";
 import PublicNavBar from "./components/NavBar/PublicNavBar";
 import SideBar from "./components/PageElement/SideBar";
@@ -5,18 +6,31 @@ import Login from "./components/auth/Login";
 import Footer from "./components/PageElement/Footer";
 import Register from "./components/auth/Register";
 function App() {
-  const log = false;
+  const log = true;
   return (
-    <>
-      <div className="flex flex-col h-screen overflow-hidden">
-        {log ? <PrivateNavBar /> : <PublicNavBar />}
-        <div className="flex flex-1 overflow-hidden">
-          {/* <SideBar userName={"Arpit"} /> */}
-          <Register />
+    <BrowserRouter>
+      {log ? (
+        <div className="flex flex-col h-screen overflow-hidden">
+          <PrivateNavBar />
+          <div className="flex flex-1 overflow-hidden">
+            <SideBar />
+            <Routes></Routes>
+          </div>
         </div>
-        <Footer />
-      </div>
-    </>
+      ) : (
+        <div className="flex flex-col h-screen overflow-hidden">
+          <PublicNavBar />
+          <div className="flex flex-1 overflow-hidden">
+            <Routes>
+              <Route path="/login" element={<Login />} />
+              <Route path="/register" element={<Register />} />
+              <Route path="*" element={<Navigate to="/login" />} />
+            </Routes>
+          </div>
+          <Footer />
+        </div>
+      )}
+    </BrowserRouter>
   );
 }
 
--- frontend/src/components/NavBar/PublicNavBar.jsx
+++ frontend/src/components/NavBar/PublicNavBar.jsx
@@ -1,6 +1,9 @@
 import React from "react";
+import { Link, useLocation } from "react-router-dom";
 
 function PublicNavBar() {
+  const location = useLocation();
+  const isLogin = location.pathname === "/login";
   return (
     <header>
       <nav className="flex items-center justify-between w-full p-3 bg-[var(--bg-primary)]">
@@ -15,9 +18,11 @@ function PublicNavBar() {
             Contact
           </a>
         </div>
-        <button className="font-[Manrope] font-bold text-xs text-[var(--primary)] bg-[var(--element-bg)] px-4  py-2 rounded-full cursor-pointer hover:bg-[var(--element-bg-hover)]">
-          Create Account
-        </button>
+        <Link to={isLogin ? "/register" : "/login"}>
+          <button className="font-[Manrope] font-bold text-xs text-[var(--primary)] bg-[var(--element-bg)] px-4  py-2 rounded-full cursor-pointer hover:bg-[var(--element-bg-hover)] w-32">
+            {isLogin ? "Create Account" : "Login"}
+          </button>
+        </Link>
       </nav>
     </header>
   );
